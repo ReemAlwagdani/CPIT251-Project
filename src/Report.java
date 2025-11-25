60daa69fa4c83891a7ba74eb5be1737bce506626
LENOVO <LENOVO@DESKTOP-I8TVS9U>
2025-11-25 20:10:00 +0300

    Implement Report class (model)

--- src/Report.java
+++ src/Report.java
@@ -1,13 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author LENOVO
- */
 public class Report {
+     private int id;
+
+    private String issue;
+    private String location;
+
+    private String status;
+
+    private String submittedBy;
+
+    public Report(int id, String issue,
+                  String location, String submittedBy) {
+        this.id = id;
+        this.issue = issue;
+        this.location = location;
+        this.submittedBy = submittedBy;
+        this.status = "NEW"; // default status
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getSubmittedBy() {
+        return submittedBy;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               " | Issue: " + issue +
+               " | Class number: " + location +
+               " | Status: " + status +
+               " | By: " + submittedBy;
+    }
     
 }
