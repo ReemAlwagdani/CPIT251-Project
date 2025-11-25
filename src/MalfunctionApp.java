1eb642c4e1c2c797c96860107be3170a7d4c77bb
LENOVO <LENOVO@DESKTOP-I8TVS9U>
2025-11-25 19:46:51 +0300

    Initial commit - Malfunction System

---
+++ src/MalfunctionApp.java
@@ -0,0 +1,10 @@
+
+public class MalfunctionApp {
+
+    public static void main(String[] args) {
+          System.out.println("\t\tWelcome to Malfunction Report System!\n"
+                + "\t_______________________________________________\n");
+        
+    }
+    
+}
