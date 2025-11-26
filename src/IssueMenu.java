fec346ccfd220e3772f80725d507cef4f35394ee
Hanneennmohaammed <hanmod1999@gmail.com>
2025-11-26 04:52:55 +0300

    Implement IssueMenu (issue selection menu)

---
+++ src/IssueMenu.java
@@ -0,0 +1,41 @@
+
+/**
+ *
+ * @author Hanmo
+ */
+
+
+import java.util.Scanner;
+public class IssueMenu {
+
+    public static String chooseIssue(Scanner in) {
+        System.out.println("\nSelect the issue:");
+        System.out.println("1. Projector - No signal");
+        System.out.println("2. Projector - Lamp problem");
+        System.out.println("3. PC - Not turning on");
+        System.out.println("4. PC - Very slow");
+        System.out.println("5. Network - No internet");
+        System.out.print("Enter issue number: ");
+
+        int issueChoice = in.nextInt();
+        in.nextLine();
+
+        switch (issueChoice) {
+            case 1:
+                return "Projector - No signal";
+            case 2:
+                return "Projector - Lamp problem";
+            case 3:
+                return "PC - Not turning on";
+            case 4:
+                return "PC - Very slow";
+            case 5:
+                return "Network - No internet";
+            default:
+                return null;
+        }
+    }
+
+}
+
+
